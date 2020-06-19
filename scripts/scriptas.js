b42d0fde8ab919b395bbc31988242df73ac67791
gratax <67147546+gratax@users.noreply.github.com>
2020-06-19 15:24:31 +0300

    Kursinis darbas

---
+++ scripts/scriptas.js
@@ -0,0 +1,29 @@
+$('#modal1').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal1 iframe').attr("src", $("#modal1 iframe").attr("src"));
+});
+
+$('#modal6').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal6 iframe').attr("src", $("#modal6 iframe").attr("src"));
+});
+
+$('#modal4').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal4 iframe').attr("src", $("#modal4 iframe").attr("src"));
+});
+
+$('#modal2').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal2 iframe').attr("src", $("#modal2 iframe").attr("src"));
+});
+
+$('#modal3').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal3 iframe').attr("src", $("#modal3 iframe").attr("src"));
+});
+
+$('#modal5').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal5 iframe').attr("src", $("#modal5 iframe").attr("src"));
+});
